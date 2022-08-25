b2db3d7f716be22b1f4636fb6affa91f0d72d44e
iridion <davide.antinori@moveax.it>
2022-08-25 13:32:12 +0200

    Add icons

--- app/app.js
+++ app/app.js
@@ -331,7 +331,7 @@ angular.module('myApp', [
         }])
 
     .component('backToListBtn', {
-        template: `<a href="#!/list" type="button" class="btn btn-secondary mr-2">Torna alla lista</a>`,
+        template: `<a href="#!/list" type="button" class="btn btn-secondary mr-2"><i class="fa-solid fa-arrow-left"></i> Torna alla lista</a>`,
         controller: function () {
         },
     })
